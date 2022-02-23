972f4680fbf8489d1460fba1cb9fc9a05d94aac0
François Déchelle <fdechelle@teclib.com>
2022-02-23 17:38:52 +0100

    Add writing JSON to file

--- Tests/IDMEFTests/IDMEFSerializeTests.swift
+++ Tests/IDMEFTests/IDMEFSerializeTests.swift
@@ -1,17 +1,29 @@
 import XCTest
 @testable import IDMEF
 
+func serialize(msg: IDMEFObject, outFile: String) {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    if let jsonData = try? encoder.encode(msg) {
+        if let jsonString = String(data: jsonData, encoding: .utf8) {
+            do {
+                try jsonString.write(to: URL(fileURLWithPath: outFile), atomically: true, encoding: String.Encoding.utf8)
+            } catch {
+                XCTFail()
+            }            
+        }
+    }
+    
+    XCTAssert(true)
+
+}
+
 final class IDMEFSerializeTests: XCTestCase {
     func test1() throws {
-        let msg = message1()
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-        if let jsonData = try? encoder.encode(msg) {
-            if let jsonString = String(data: jsonData, encoding: .utf8) {
-                print(jsonString)
-            }
-        }
-        
-        XCTAssert(true)
+        serialize(msg: message1(), outFile: "out1.json")
     } 
+
+    func test2() throws {
+        serialize(msg: message2(), outFile: "out2.json")
+    }
 }
